f95ea1e0c43322cc60debe41fdba21723eae27f8
Ohm Attaphong <ohm.attaphong@gmail.com>
2016-12-07 00:23:57 +0700

    Modify functions for const URI

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+define ("DOMIN_THEME_URI", get_template_directory_uri());
 
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
