04b25b718cdc57fdc15010995861795fb8a8f02a
Ohm Attaphong <ohm.attaphong@gmail.com>
2016-12-08 00:46:27 +0700

    Restucture project for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,6 @@
-// including plugins
-var gulp = require('gulp')
-, path = require('path')
-, cssmin = require('gulp-cssmin')
-, gutil = require('gulp-util')
-, less = require("gulp-less");
+var gulp = require('gulp');
 
-gulp.task('prod', ['compile-less', 'uglify-js'], function() {
-  return gutil.log('[Stage] Gulp is running completed!')
+gulp.task('build',function(){
+	return gulp.src('./src/**/*.php').pipe(gulp.dest('./build'));
 });
 
-// task
-gulp.task('compile-less', function () {
-    return gulp.src('./less/site.less')
-    .pipe(less({}))
-    .on('error', onError)
-    .pipe(cssmin().on('error', onError))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('./less/**/*.less', ['compile-less']);
-});
-
-function onError(err) {
-  console.log(err);
-  this.emit('end');
-}
