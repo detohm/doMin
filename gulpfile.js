f14277c0d6d17b096612014643665cc5bb446d84
Ohm Attaphong <ohm.attaphong@gmail.com>
2016-12-09 23:31:17 +0700

    Add gulp method for main stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('build',function(){
+gulp.task('build-php',function(){
 	return gulp.src('./src/**/*.php').pipe(gulp.dest('./build'));
 });
+
+gulp.task('build-main-style',function(){
+	return gulp.src('./src/style.css').pipe(gulp.dest('./build'));
+});
+
+gulp.task('build',['build-php','build-main-style']);
 
